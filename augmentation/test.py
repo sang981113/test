4a358b4288cbd5d771451fa2aab5318dd6bb067e
KIM YOUSANG <yskim@euclidsoft.co.kr>
2024-04-16 13:03:52 +0900

    테스트

---
+++ augmentation/test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+class Test:
+
+    def __init__(self) -> None:
+        pass
+
+    def random(self):
+        return np.random.rand()
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test',
+    version='0.1',
+    description='test pip install',
+    url='https://gitlab.euso.kr/add/augmentation_for_pip',
+    packages=find_packages()
+)
